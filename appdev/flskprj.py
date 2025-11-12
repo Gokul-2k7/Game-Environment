789935b3510971d9de8a47c4bd51fc555ca49c32
Gokul-2k7 <k.gokul27112007@gmail.com>
2025-11-12 16:59:39 +0530

    new file added

---
+++ appdev/flskprj.py
@@ -0,0 +1,32 @@
+from flask import Flask,request,render_template,redirect,url_for
+app=Flask(__name__)
+login={}
+@app.route('/',methods=["GET","POST"])
+def home():
+    error=None
+    if request.method=="POST":
+        name=request.form.get("name")
+        Password=request.form.get("Pass")
+        if name in login and login[name]==Password:
+            return f"HI {name}"
+        else:
+           error="Invalid username or password"        
+    return render_template('index.html',error=error)
+@app.route('/register',methods=["GET","POST"])
+def register():
+    Name=None
+    if request.method=="POST":
+        Name=request.form.get("Name")
+        Pass=request.form.get("Pass")
+        if Name in login:
+            error="Username exists"
+            return render_template('register.html',login=login,Name=Name,error=error)
+        login[Name]=Pass
+        return redirect(url_for('home'))
+    return render_template('register.html',login=login,Name=Name)
+
+@app.route('/name=<name>/age=<age>')
+def details(name,age):
+    return f"{name} age is {age}"
+
+app.run(debug=True,port=8000)
